4c25f86cd361e16fdfd05d05b3da043a8587dce1
Vinh Nguyen <quangvinh0972@gmail.com>
2023-06-13 17:19:56 +0700

    ok

--- tests/integration/components/login-test.js
+++ tests/integration/components/login-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'test-app/tests/helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import { render, find, findAll, fillIn } from '@ember/test-helpers';
+import { render, findAll, fillIn, click, find } from '@ember/test-helpers';
 
 module('Integration | Component | login', function (hooks) {
   setupRenderingTest(hooks);
@@ -17,16 +17,35 @@ module('Integration | Component | login', function (hooks) {
     let nameWrapperContent = labelElms.find(e => e.textContent.includes('Username'));
     // check exist username wrapper
     assert.equal( !!nameWrapperContent, true );
-
     // if exist username container, check it has content is 'Username'
     assert.equal(nameWrapperContent.textContent.trim(), "Username")
 
     // get element input in name wrapper
     let inputUsernameElement = nameWrapperContent.querySelector('input');
     // fill value is abcd to this input
-    fillIn(inputUsernameElement, 'abcd');
-    // check input value, expect equal 'abcd'
-
+    await fillIn(inputUsernameElement, 'abcd');
+    assert.equal(inputUsernameElement.value, 'abcd')
     
+    // get label contain 'Password'
+    let pwWrapperContent = labelElms.find(e => e.textContent.includes('Password'));
+    // check exist username wrapper
+    assert.equal( !!pwWrapperContent, true );
+    // if exist password container, check it has content is 'Password'
+    assert.equal(pwWrapperContent.textContent.trim(), "Password")
+    // get element input in name wrapper
+    let inputPasswordElement = pwWrapperContent.querySelector('input');
+    // fill value is abcd to this input
+    await fillIn(inputPasswordElement, 1234);
+    assert.equal(inputPasswordElement.value, 1234)
+    
+    // Find the button element
+    const buttonElement = find('button');
+    // Click the button element
+    await click(buttonElement);
+
+    const sttSuccess = find('#sign-in-success');
+    assert.equal(!!sttSuccess, true)
+    // console.log('111', sttSuccess.textContent)
+    assert.equal(sttSuccess.textContent.trim() , "Login success !!!" )
   });
 });
